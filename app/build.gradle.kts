74eedd2bc29a319bcace0f6fac8dc4edab2536fc
Melipilla <melipilla@duoc.acad>
2025-10-01 15:31:24 -0300

    Se agregaron las dependencias para trabajar la navegación

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,10 @@ android {
 
 dependencies {
 
+    implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.kotlin.coroutines.android)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
