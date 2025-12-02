d22b2d6465716587a9063560eae34eb528ff0ca3
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2025-12-02 15:28:28 -0300

    caca

--- app/src/main/java/com/example/tienda_bonbin/data/NetworkModule.kt
+++ app/src/main/java/com/example/tienda_bonbin/data/NetworkModule.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object NetworkModule {
-    private const val BASE_URL = "http://192.168.1.11:8081/"
+    private const val BASE_URL = "http://10.57.68.46:8081/"
     private var apiService: ApiService? = null
 
     /**
@@ -25,7 +25,8 @@ object NetworkModule {
             val authInterceptor = AuthInterceptor(sessionRepository) // <--- ¡Usa el que viene como parámetro!
 
             // C, D, E y F se quedan exactamente igual...
-            val loggingInterceptor = HttpLoggingInterceptor().apply {
+            //Esto Activa el Logcat cuando la aplicacion se esta emulando en el dispositivo
+            val loggingInterceptor = HttpLoggingInterceptor().apply  {
                 level = HttpLoggingInterceptor.Level.BODY
             }
 
